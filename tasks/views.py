b880c3fd03029e9d0022660eb4d775c2dbab6a93
Vineet <vineetkushwaha6325@gmail.com>
2025-05-12 22:28:35 +0530

    hehe almost2

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 
 # Create your views here.
-
+@xframe_options_exempt
 def index(request):
     return render(request, 'tasks/index.html')
 
